97ef12e498fcfd5a77809b3f9426b3f95dcea62b
urevoleg <urevolegg@gmail.com>
2022-12-01 22:07:03 +0300

    add to_wav file

--- src/ml/app/utils.py
+++ src/ml/app/utils.py
@@ -26,7 +26,7 @@ def get_model():
 
 def prepare_audio_file(audio_file_path: str):
     mp3 = AudioSegment.from_file(audio_file_path)
-    if audio_file_path.split(".")[-1] == 'wav':
+    if audio_file_path.split(".")[-1] == 'wav_':
         #TODO необходимо, тк *.wav файлы от телеграма по-умолчанию, выдают пустоту
         # ниже выполняется их пересохранение в mp3
         mp3.export(audio_file_path.replace('.wav', '.mp3'), format="mp3")
